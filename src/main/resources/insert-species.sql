1a960b357db2547a5fb96e583c276048a8a9c16d
Joseph D'Anna <jodyanna14.15@gmail.com>
2021-11-03 14:09:30 -0500

    removed boiler plate comments, updated sql insert file, added favicon

---
+++ src/main/resources/insert-species.sql
@@ -0,0 +1,69 @@
+
+-- Fake species
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('DOG');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('CAT');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('BIRD');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('FISH');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('GOAT');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('PIG');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('SHEEP');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('COW');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('DONKEY');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('HORSE');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('FERRET');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('RABBIT');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('RODENT');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('INSECT');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('REPTILE');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('FOX');
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ('OTHER');
